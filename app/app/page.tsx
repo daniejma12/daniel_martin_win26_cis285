a996fa88cf11cd3246483dee67f10846e95522b5
daniejma12 <daniejma@umich.edu>
2026-04-28 19:31:22 +0000

    added sentence change and date

--- app/app/page.tsx
+++ app/app/page.tsx
@@ -17,7 +17,7 @@ export default function Home() {
             CIS 285 Final Exam
           </h1>
           <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
+            This sentence change was made on the codespace! on 4/28{" "}
             <a
               href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
               className="font-medium text-zinc-950 dark:text-zinc-50"
